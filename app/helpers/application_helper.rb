3c3ee068007da22deaeb5ea641de8ed182521acb
ckalan <ckalan@gmail.com>
2017-02-04 05:46:03 +0300

    Add a page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  DefaultTitle = "Ruby on Rails Tutorial Sample App"
+
+  def page_title(title="")
+     (title.empty? ? "" : (title + " | ") ) + DefaultTitle
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    # @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{ApplicationHelper::DefaultTitle}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | #{ApplicationHelper::DefaultTitle}"
   end
 
   test "should get about" do
 
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | #{ApplicationHelper::DefaultTitle}"
 
   end
 
