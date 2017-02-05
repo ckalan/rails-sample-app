726929fa76a08e62ac93d5dc049e12504ef7ce21
ckalan <ckalan@gmail.com>
2017-02-05 03:58:51 +0300

    Fix commit error

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get users_new_url
+    assert_response :success
+  end
+
+  test "should get sign_up" do
+    get signup_path
+    assert_response :success
+  end
+
+
+end
